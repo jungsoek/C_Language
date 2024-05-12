47130875a57a7fa3211dd994b53882782caba385
ojsicn <jungsoeko@gmail.com>
2024-05-12 15:47:21 +0900

    10_1 배열과 포인터와의 관계 마무리

---
+++ codes/chap10_arraypointer/10_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int ary[3] = {10, 20, 30};
+    int *pa = ary;
+    int i;
+
+    printf("배열의 값 : ");
+    for(i = 0; i < 3; i++) {
+        printf("\t%d", *pa);
+        pa++;
+    }
+
+    return 0;
+}
